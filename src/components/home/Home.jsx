45994a2493287ff40bda20569f7cdd84ff8cd79a
Harshal997 <harshalkher27@gmail.com>
2024-12-24 21:21:27 +0530

    Added save and prompt functionality

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,6 +5,7 @@ import Prompt from "../prompt/Prompt";
 import Chats from "../chats/Chats";
 import data from "../../data.json";
 import Feedback from "../feedback/Feedback";
+import Nav from "../nav/Nav";
 
 const prompts = [
   "Hi, what is the weather",
@@ -13,84 +14,119 @@ const prompts = [
   "Hi, how are you",
 ];
 
+const handleSave = (chats) => {
+  if (chats.length === 0) {
+    alert("Please start a conversation to save!");
+    return;
+  }
+  const data = localStorage.getItem("chats");
+  const storedConversations = data ? JSON.parse(data) : [];
+  const newStoredData = [...storedConversations, ...chats];
+  localStorage.setItem("chats", JSON.stringify(newStoredData));
+  alert("Saved Successfully!");
+};
+
+export const handleAsk = (setChats, setHasAsked, setPrompt, prompt) => {
+  if (prompt === "") {
+    alert("Please type something!");
+    return;
+  }
+  console.log(prompt);
+  setChats((prevChats) => [
+    ...prevChats,
+    {
+      id: Date.now(),
+      user: true,
+      prompt: prompt,
+      time: new Date().getHours() + ":" + new Date().getMinutes(),
+    },
+  ]);
+  const matchedQuestion = data.find(
+    (item) => item.question.toLowerCase() === prompt.toLowerCase()
+  );
+
+  setChats((prevChats) => [
+    ...prevChats,
+    {
+      id: Date.now(),
+      user: false,
+      prompt: matchedQuestion
+        ? matchedQuestion.response
+        : "As an AI Language Model, I don’t have the details",
+      time: new Date().getHours() + ":" + new Date().getMinutes(),
+    },
+  ]);
+  setHasAsked(true);
+  setPrompt("");
+};
+
 const Home = () => {
   const [chats, setChats] = useState([]);
   const [prompt, setPrompt] = useState("");
   const [hasAsked, setHasAsked] = useState(false);
   const [feedbackVisible, setFeedbackVisible] = useState(false);
 
-  const handleAsk = (e) => {
-    if (prompt === "") {
-      alert("Please type something!");
-      return;
-    }
-    console.log(prompt);
-    setChats((prevChats) => [
-      ...prevChats,
-      {
-        id: Date.now(),
-        user: true,
-        prompt: prompt,
-        time: new Date().getHours() + ":" + new Date().getMinutes(),
-      },
-    ]);
-    const matchedQuestion = data.find(
-      (item) => item.question.toLowerCase() === prompt.toLowerCase()
-    );
-
-    setChats((prevChats) => [
-      ...prevChats,
-      {
-        id: Date.now(),
-        user: false,
-        prompt: matchedQuestion
-          ? matchedQuestion.response
-          : "As an AI Language Model, I don’t have the details",
-        time: new Date().getHours() + ":" + new Date().getMinutes(),
-      },
-    ]);
-    setHasAsked(true);
-    setPrompt("");
-  };
-
   return (
-    <div className="container">
-      {feedbackVisible && (
-        <div className="feedback">
-          <Feedback visible={feedbackVisible} setVisible={setFeedbackVisible} />
-        </div>
-      )}
-      <h1>Bot AI</h1>
-      {!hasAsked ? (
-        <>
-          <div className="heading">
-            <h1>How Can I Help You Today</h1>
-            <img src={logo} alt="" />
+    <div>
+      <Nav />
+      <div className="container">
+        {feedbackVisible && (
+          <div className="feedback">
+            <Feedback
+              visible={feedbackVisible}
+              setVisible={setFeedbackVisible}
+            />
           </div>
-          <div className="prompts-container">
-            {prompts &&
-              prompts.length > 0 &&
-              prompts.map((prompt, index) => (
-                <Prompt key={index} prompt={prompt} />
+        )}
+        <h1>Bot AI</h1>
+        {!hasAsked ? (
+          <>
+            <div className="heading">
+              <h1>How Can I Help You Today</h1>
+              <img src={logo} alt="" />
+            </div>
+            <div className="prompts-container">
+              {prompts &&
+                prompts.length > 0 &&
+                prompts.map((prompt, index) => (
+                  <Prompt
+                    setChats={setChats}
+                    setPrompt={setPrompt}
+                    setHasAsked={setHasAsked}
+                    key={index}
+                    prompt={prompt}
+                  /> // setChats, setHasAsked, setPrompt, prompt
+                ))}
+            </div>
+          </>
+        ) : (
+          <div className="chats">
+            {chats &&
+              chats.length &&
+              chats.map((chat) => (
+                <Chats
+                  key={chat.id}
+                  chat={chat}
+                  visible={feedbackVisible}
+                  setVisible={setFeedbackVisible}
+                />
               ))}
           </div>
-        </>
-      ) : (
-        <div className="chats">
-          {chats &&
-            chats.length &&
-            chats.map((chat) => <Chats key={chat.id} chat={chat} visible={feedbackVisible} setVisible={setFeedbackVisible} />)}
+        )}
+        <div className="input-query">
+          <input
+            value={prompt}
+            type="text"
+            className="input"
+            onChange={(e) => setPrompt(e.target.value)}
+          />
+          <button
+            onClick={() => handleAsk(setChats, setHasAsked, setPrompt, prompt)}
+          >
+            Ask
+          </button>
+          <button onClick={() => handleSave(chats)}>Save</button>
         </div>
-      )}
-      <div className="input-query">
-        <input
-          value={prompt}
-          type="text"
-          className="input"
-          onChange={(e) => setPrompt(e.target.value)}
-        />
-        <button onClick={handleAsk}>Ask</button>
-        <button>Save</button>
       </div>
     </div>
   );
--- src/components/prompt/Prompt.jsx
+++ src/components/prompt/Prompt.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './prompt.css'
+import { handleAsk } from '../home/Home'
 
-const Prompt = ({prompt}) => {
+const Prompt = ({prompt, setHasAsked, setPrompt, setChats}) => {
   return (
-    <div className='prompt-container'>
+    <div className='prompt-container' onClick={() => handleAsk(setChats, setHasAsked, setPrompt, prompt)}>
         <h3>{prompt}</h3>
         <p>Get immediate AI generated response</p>
     </div>
